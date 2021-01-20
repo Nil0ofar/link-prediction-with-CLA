5cb7b76ad88b296c81aab1dd14036bbcdfa09665
Nil0ofar <torkamani_77@yahoo.com>
2021-01-19 21:50:39 -0800

    update method for reading files

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from cla import CLA
+import os
+
 node_num = 900
-step_num = 6
 reward = 0.04
 penalty = 0.01
 iteration = 1
+data_path = "./data/"
 
 cla = CLA(node_num , reward , penalty , iteration)
 
-for idx in range(0 ,step_num) :
-    f = open("{}.txt".format(idx), 'r')
-    mat = [[0 for i in range(node_num)]for j in range(node_num)]
-    for line in f:
-        x, y = map(int, line.split(' '))
-        mat[x][y] = mat[y][x] = 1
+for filename in os.listdir(data_path):
+    with open(data_path + filename , 'r') as f:
+        mat = [[0 for i in range(node_num)]for j in range(node_num)]
+        for line in f:
+            x, y = map(int, line.split(' '))
+            mat[x][y] = mat[y][x] = 1
 
-    cla.update(mat)
-    print("AUC : " + str(cla.predict(mat)))
+        cla.update(mat)
+        print("AUC : " + str(cla.predict(mat)))
 
 
 
